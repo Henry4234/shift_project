d81a73a8b4654c7ae949e964c9cc351289419a0c
Henry4234 <102476562+Henry4234@users.noreply.github.com>
2025-06-03 00:04:03 +0800

    V3.2 [修改].gitignore [新增].env，加入SUPABASE_URL & SUPABASE_KEY [新增]dockerfile，RUN npm dotenv [修改]supabase.js，隱藏URL & TOKEN

--- web/supabase.js
+++ web/supabase.js
@@ -1,5 +1,11 @@
 import { createClient } from '@supabase/supabase-js'
-const supabaseUrl = 'https://iadhgslrubguevwtqsib.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlhZGhnc2xydWJndWV2d3Rxc2liIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg3NzE5OTAsImV4cCI6MjA2NDM0Nzk5MH0.kyPhBczr4xo8CCC5cHeRcKbPquhHpU_3w2BQ9pSB7LE'
+
+// 載入 .env 變數（只有 Node.js 有效，瀏覽器無效）
+import dotenv from 'dotenv'
+dotenv.config()
+
+
+const supabaseUrl = process.env.SUPABASE_URL
+const supabaseKey = process.env.SUPABASE_KEY
 const supabase = createClient(supabaseUrl, supabaseKey)
 export { supabase }
